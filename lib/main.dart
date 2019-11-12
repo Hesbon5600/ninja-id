b4f8f0301f4ed354853f3b22a3a755355a27d04d
Hesbon5600 <hesbonkiptoo5600@gmail.com>
2019-11-12 17:26:01 +0300

    use stateful component

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       home: NinjaCard(),
     ));
+  
 
-    
+
 class NinjaCard extends StatefulWidget {
   @override
   _NinjaCardState createState() => _NinjaCardState();
